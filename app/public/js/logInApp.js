ccec31bffffc8fe4fb3500f3eb7120c4a8e5a456
MateoC <carvajalmunoz@gmail.com>
2014-09-12 17:18:26 -0500

    login con facebook

--- app/public/js/logInApp.js
+++ app/public/js/logInApp.js
@@ -6,18 +6,28 @@ app.controller('bodyController',
         '$http',
         '$window',
         function($scope, $http, $window){
+            
             $scope.logIn = function(){
-                $http.post('/logIn', {userId : 666}).
-                    success(
-                        function(data, status){
-                            $window.location.href = '/home.html';
-                        }
-                    ).
-                    error(
-                        function(data, status){
+               FB.login(function(response) {
+                 if (response.authResponse) {
+                    $http.post('/login', {userId : response.id}).
+                        success(
+                            function(data, status){
+                                $window.location.href = '/home.html';
+                            }
+                        ).
+                        error(
+                            function(data, status){
                             
-                        }
-                    );
+                            }
+                        );
+                  } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                  }
+              }, {scope: 'email'});
+
+                /*
+                */
             };
         }
     ]
