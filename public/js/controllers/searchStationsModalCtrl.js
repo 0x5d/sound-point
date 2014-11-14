97091c3a476367f4bea80750214000c4a2619880
Jose Fernando Herrera <jfernandoherrera1@gmail.com>
2014-11-13 22:19:56 -0500

    error al buscar estaciones sin haber creado alguna antes

--- public/js/controllers/searchStationsModalCtrl.js
+++ public/js/controllers/searchStationsModalCtrl.js
@@ -27,28 +27,27 @@ app.controller('searchStationsModalCtrl', [
                    $http.get('/public/' + $scope.items[i].id).
                                     success(
                                         function(data, status){
-                                       var count=0;
+                                        var count=0;
                                         var j=0;
-                                        var k=0;
-                                       
+                                        var k=0;       
+                                        if(stations.length==0){
+                                            $scope.publicStations = data.stations; 
+                                        }else{ 
                                         while(j<data.stations.length){
-                                         while(k<stations.length){    
-                                       console.log(data.stations[k]._id);
-                                       console.log(stations[k]._id);
-                                          if(data.stations[j]._id==stations[k]._id){                                              
-                                           break;   
-                                          }else if(k==(stations.length-1)){
-                                             $scope.publicStations[count]= data.stations[j];
-                                             count++;
+                                            while(k<stations.length){    
+                                            if(data.stations[j]._id==stations[k]._id){                                              
+                                            break;   
+                                            }else if(k==(stations.length-1)){
+                                            $scope.publicStations[count]= data.stations[j];
+                                            count++;
                                           }
                                           k++;
                                         }
                                         k=0;
                                         j++;
-                                            //$scope.publicStations = data.stations;                                             
-                                        
                                         }
                                     }
+                                }
                                     ).
                                     error(
                                         function(data, status){   
